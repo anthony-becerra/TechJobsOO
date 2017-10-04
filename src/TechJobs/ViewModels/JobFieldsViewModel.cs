7af4c7f512aeaf72492df829547d7c078206a81a
Anthony Becerra <abece005@fiu.edu>
2017-10-04 14:53:50 -0400

    Finished tasks 6 & 7

--- src/TechJobs/ViewModels/JobFieldsViewModel.cs
+++ src/TechJobs/ViewModels/JobFieldsViewModel.cs
@@ -4,7 +4,7 @@ using TechJobs.Models;
 
 namespace TechJobs.ViewModels
 {
-    public class JobFieldsViewModel
+    public class JobFieldsViewModel : BaseViewModel
     {
         // TODO #7.2 - Extract members common to SearchJobsViewModel
         // to BaseViewModel
@@ -18,21 +18,5 @@ namespace TechJobs.ViewModels
         // All columns, for display
         public List<JobFieldType> Columns { get; set; }
 
-        // View title
-        public string Title { get; set; } = "";
-
-        public JobFieldsViewModel()
-        {
-            // Populate the list of all columns
-
-            Columns = new List<JobFieldType>();
-
-            foreach (JobFieldType enumVal in Enum.GetValues(typeof(JobFieldType)))
-            {
-                Columns.Add(enumVal);
-            }
-
-
-        }
     }
 }
